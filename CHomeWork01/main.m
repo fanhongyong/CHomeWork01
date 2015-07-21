380d2e3f2c08a20128cbf7af36907a7b3d77b567
zuoyoulian <zuoyoulian@126.com>
2015-07-21 23:43:49 +0800

    Initial Commit

---
+++ CHomeWork01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CHomeWork01
+//
+//  Created by 左建军 on 15/7/21.
+//  Copyright (c) 2015年 lanou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
